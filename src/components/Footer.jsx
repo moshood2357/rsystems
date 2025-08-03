3df484e01bd5f66c7b648d677c2e81a35df75f61
Moshood <sanusimoshoodabiola@gmail.com>
2025-08-03 10:36:18 +0100

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -78,7 +78,7 @@ export default function Footer() {
         </div>
 
         <div className="border-t border-gray-800 mt-8 pt-8 text-center text-gray-400">
-          <p>&copy; 2024 R2 System Solution Ltd. All rights reserved.</p>
+          <p>&copy; 2024 R2 System Solution Ltd. All rights reserved</p>
         </div>
       </div>
     </footer>
